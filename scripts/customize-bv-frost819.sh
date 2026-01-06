28b811037f5b5fb32a1803ec65520b117645a4a6
zxg <zhouleike@live.com>
2026-01-06 23:05:37 +0800

    更新

--- scripts/customize-bv-frost819.sh
+++ scripts/customize-bv-frost819.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # customize-bv-frost819.sh
-# 简化版本：直接修改 AppConfiguration.kt 文件中的配置
 
+FROST819_BV_SOURCE_ROOT="$GITHUB_WORKSPACE/frost819-bv-source"
+
+FROST819_BV_SOURCE_APPCONFIGURATION="$FROST819_BV_SOURCE_ROOT/buildSrc/src/main/kotlin/AppConfiguration.kt"
 # 修改 AppConfiguration.kt 中的两个配置项
 sed -i \
   -e 's/const val minSdk = 23/const val minSdk = 21/' \
   -e 's/"git rev-list --count HEAD".exec().toInt() - 5/"git rev-list --count HEAD".exec().toInt() + 1/' \
-  buildSrc/src/main/kotlin/AppConfiguration.kt
+  "$FROST819_BV_SOURCE_APPCONFIGURATION"
