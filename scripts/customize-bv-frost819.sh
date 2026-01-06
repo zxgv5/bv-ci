b216c18075442f973e08b4433113e71de713a208
zxgv5 <zhouleike@live.com>
2026-01-06 22:01:03 +0800

    更新

--- scripts/customize-bv-frost819.sh
+++ scripts/customize-bv-frost819.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 # customize-bv-frost819.sh
-# 用于对源代码进行构建前的自定义修改
+# 简化版本：直接修改 AppConfiguration.kt 文件中的配置
 
-echo "开始执行自定义构建前修改脚本..."
-
-# 1. 修改minSdk从23到21
-sed -i 's/const val minSdk = 23/const val minSdk = 21/g' buildSrc/src/main/kotlin/AppConfiguration.kt
-echo "✓ 已将minSdk从23修改为21"
-
-# 2. 这里可以添加更多修改，例如：
-# sed -i 's/其他需要替换的内容/新内容/g' 目标文件路径
-# 或者执行其他构建前准备任务
-
-echo "自定义修改完成"
+# 修改 AppConfiguration.kt 中的两个配置项
+sed -i \
+  -e 's/const val minSdk = 23/const val minSdk = 21/' \
+  -e 's/"git rev-list --count HEAD".exec().toInt() - 5/"git rev-list --count HEAD".exec().toInt() + 1/' \
+  buildSrc/src/main/kotlin/AppConfiguration.kt
