770754e5e1dba017acd2b9ddde91c31c516db2ec
zxgv5 <zhouleike@live.com>
2026-01-22 00:34:48 +0800

    更新

--- patches/bv_fantasy/ci_RecommendViewModel.kt
+++ patches/bv_fantasy/ci_RecommendViewModel.kt
@@ -5,7 +5,7 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
-import dev.aaa1115910.biliapi.entity.rank.PopularVideoPage
+import dev.aaa1115910.biliapi.entity.home.RecommendPage
 import dev.aaa1115910.biliapi.entity.ugc.UgcItem
 import dev.aaa1115910.biliapi.repositories.RecommendVideoRepository
 import dev.aaa1115910.bv.BVApp
@@ -20,69 +20,78 @@ import kotlinx.coroutines.withContext
 import org.koin.android.annotation.KoinViewModel
 
 @KoinViewModel
-class PopularViewModel(
+class RecommendViewModel(
     private val recommendVideoRepository: RecommendVideoRepository
 ) : ViewModel() {
     private val logger = KotlinLogging.logger {}
-    val popularVideoList = mutableStateListOf<UgcItem>()
+    val recommendVideoList = mutableStateListOf<UgcItem>()
 
-    private var nextPage = PopularVideoPage()
-    var refreshing by mutableStateOf(false)
+    private var nextPage = RecommendPage()
+    var refreshing by mutableStateOf(true)
     var loading by mutableStateOf(false)
     var hasMore by mutableStateOf(true) // 添加hasMore状态
-
-    init {
-        println("=====init PopularViewModel")
-    }
 
     suspend fun loadMore(
         beforeAppendData: () -> Unit = {}
     ) {
-        if (!loading && hasMore) loadData( // 添加hasMore检查
-            beforeAppendData = beforeAppendData
-        )
+        var loadCount = 0
+        val maxLoadMoreCount = 3
+        if (!loading && hasMore) { // 添加hasMore检查
+            if (recommendVideoList.size == 0) {
+                // first load data
+                while (recommendVideoList.size < 24 && loadCount < maxLoadMoreCount && hasMore) {
+                    val emptyFun: () -> Unit = {}
+                    loadData(beforeAppendData = if (loadCount == 0) beforeAppendData else emptyFun)
+                    if (loadCount != 0) logger.fInfo { "Load more recommend videos because items too less" }
+                    loadCount++
+                }
+            } else {
+                val emptyFun: () -> Unit = {}
+                loadData(beforeAppendData = if (loadCount == 0) beforeAppendData else emptyFun)
+            }
+        }
     }
 
     private suspend fun loadData(
         beforeAppendData: () -> Unit
     ) {
         loading = true
-        logger.fInfo { "Load more popular videos" }
+        logger.fInfo { "Load more recommend videos" }
         runCatching {
-            val popularVideoData = recommendVideoRepository.getPopularVideos(
+            val recommendData = recommendVideoRepository.getRecommendVideos(
                 page = nextPage,
                 preferApiType = Prefs.apiType
             )
             beforeAppendData()
             
             // 获取新数据
-            val newItems = popularVideoData.list
+            val newItems = recommendData.items
             
             // 如果没有获取到新数据，则认为没有更多了
             if (newItems.isEmpty()) {
                 hasMore = false
             } else {
                 // 更新下一页参数
-                nextPage = popularVideoData.nextPage
-                popularVideoList.addAllWithMainContext(newItems)
+                nextPage = recommendData.nextPage
+                recommendVideoList.addAllWithMainContext(newItems)
             }
         }.onFailure {
-            logger.fError { "Load popular video list failed: ${it.stackTraceToString()}" }
+            logger.fError { "Load recommend video list failed: ${it.stackTraceToString()}" }
             withContext(Dispatchers.Main) {
-                "加载热门视频失败: ${it.localizedMessage}".toast(BVApp.context)
+                "加载推荐视频失败: ${it.localizedMessage}".toast(BVApp.context)
             }
         }
         loading = false
     }
 
     fun clearData() {
-        popularVideoList.clear()
+        recommendVideoList.clear()
         resetPage()
         loading = false
     }
 
     fun resetPage() {
-        nextPage = PopularVideoPage()
+        nextPage = RecommendPage()
         refreshing = true
         hasMore = true // 重置hasMore状态
     }
