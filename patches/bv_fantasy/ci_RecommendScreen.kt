81354b69b87077fe952866b4ed0783f319aba5a7
zxgv5 <zhouleike@live.com>
2026-01-22 00:32:05 +0800

    更新

--- patches/bv_fantasy/ci_RecommendScreen.kt
+++ patches/bv_fantasy/ci_RecommendScreen.kt
@@ -51,8 +51,8 @@ fun PopularScreen(
             
             // 获取可见区域最后一个item索引
             val lastVisibleIndex = lazyGridState.layoutInfo.visibleItemsInfo.lastOrNull()?.index ?: -1
-            // 提前15项触发加载
-            if (lastVisibleIndex >= listSize - 15) {
+            // 提前24项触发加载
+            if (lastVisibleIndex >= listSize - 24) {
                 scope.launch(Dispatchers.IO) {
                     popularViewModel.loadMore()
                 }
